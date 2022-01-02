3ef52d96e621f93df1867e1f43be14d4e23e2cfd
Ricardo Fulgencio Alves <rfulgencio3@gmail.com>
2022-01-02 10:30:25 -0300

    Criando projeto Basket.API

---
+++ src/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Itens { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart() {}
+        public ShoppingCart(string userName)
+        {
+            UserName = UserName;
+        }
+
+        public decimal TotalPrice
+        {
+            get 
+            { 
+                decimal totalPrice = 0;
+                foreach (var item in Itens)
+                    {
+                        totalPrice += item.Price * item.Quantity;
+                    }
+                    return totalPrice;
+            }
+        }
+    }
+}
